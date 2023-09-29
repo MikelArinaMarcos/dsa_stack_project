7f719667f8eff90e5754a5d821232ca6af479d9f
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-09-29 10:27:42 +0200

    StackImpl creada

---
+++ src/main/java/StackImpl.java
@@ -0,0 +1,24 @@
+public class StackImpl<T> implements Stack<Integer> {
+    public StackImpl(T i) {
+    }
+
+    @Override
+    public void push(Integer integer) throws FullStackException {
+
+    }
+
+    @Override
+    public Integer pop() throws EmptyStackException {
+        return null;
+    }
+
+    @Override
+    public Integer peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
